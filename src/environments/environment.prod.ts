2f247c5f6c03ed4416ed6855104c8b0dce5da29f
walid <walid.dev@outlook.fr>
2023-04-11 18:21:19 +0200

    GitHub CD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC2ZFNZtuogKbNxA8vEWJe_7bh0OphjAfg',
+    authDomain: 'hadith-qudsi-17a8e.firebaseapp.com',
+    projectId: 'hadith-qudsi-17a8e',
+    storageBucket: 'hadith-qudsi-17a8e.appspot.com',
+    messagingSenderId: '831051472705',
+    appId: '1:831051472705:web:de4332bc8903923b230bc0'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC2ZFNZtuogKbNxA8vEWJe_7bh0OphjAfg',
+    authDomain: 'hadith-qudsi-17a8e.firebaseapp.com',
+    projectId: 'hadith-qudsi-17a8e',
+    storageBucket: 'hadith-qudsi-17a8e.appspot.com',
+    messagingSenderId: '831051472705',
+    appId: '1:831051472705:web:de4332bc8903923b230bc0'
+  }
 };
 
 /*
